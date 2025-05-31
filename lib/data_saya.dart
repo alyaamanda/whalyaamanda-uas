d0851dc15ab5bd467764a48690f020f7f080824c
Zakiyyah Putri Nazhifah <zakiyahputri00@gmail.com>
2025-05-31 23:48:34 +0700

    uas-selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "Alya";
+  static String gambar = "assets/mahasiswa/fotoalya.jpg";
 }
